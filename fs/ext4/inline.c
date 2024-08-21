9810bd95f946fbf5466dbda4ba553da0c294d89a
Thadeu Lima de Souza Cascardo <cascardo@igalia.com>
2024-08-21 12:23:22 -0300

    ext4: return error on ext4_find_inline_entry
    [ Upstream commit 4d231b91a944f3cab355fce65af5871fb5d7735b ]
    
    In case of errors when reading an inode from disk or traversing inline
    directory entries, return an error-encoded ERR_PTR instead of returning
    NULL. ext4_find_inline_entry only caller, __ext4_find_entry already returns
    such encoded errors.
    
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@igalia.com>
    Link: https://patch.msgid.link/20240821152324.3621860-3-cascardo@igalia.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Stable-dep-of: c6b72f5d82b1 ("ext4: avoid OOB when system.data xattr changes underneath the filesystem")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit ce8f41fca0b6bc69753031afea8fc01f97b5e1af)
    Signed-off-by: Siva Balasubramanian <sivakumar.bs@gmail.com>

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -1689,7 +1689,7 @@ struct buffer_head *ext4_find_inline_entry(struct inode *dir,
 	void *inline_start;
 	int inline_size;
 
-	ret = ext4_get_inode_loc(dir, &is.iloc);
+	ret = ext4_get_inode_loc(dir, &iloc);
 	if (ret)
 		return ERR_PTR(ret);
 
@@ -1726,11 +1726,11 @@ struct buffer_head *ext4_find_inline_entry(struct inode *dir,
 		goto out_find;
 
 out:
-	brelse(is.iloc.bh);
+	brelse(iloc.bh);
 	if (ret < 0)
-		is.iloc.bh = ERR_PTR(ret);
+		iloc.bh = ERR_PTR(ret);
 	else
-		is.iloc.bh = NULL;
+		iloc.bh = NULL;
 out_find:
 	up_read(&EXT4_I(dir)->xattr_sem);
 	return is.iloc.bh;
